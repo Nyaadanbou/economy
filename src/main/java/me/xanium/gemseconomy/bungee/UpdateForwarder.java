933c91b7ce9a602ce82ece35b23997d5ac9481f2
Nailm <porridgen@gmail.com>
2022-07-22 15:33:46 +0800

    IDEA format

--- src/main/java/me/xanium/gemseconomy/bungee/UpdateForwarder.java
+++ src/main/java/me/xanium/gemseconomy/bungee/UpdateForwarder.java
@@ -11,6 +11,7 @@ import me.xanium.gemseconomy.utils.UtilServer;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.messaging.PluginMessageListener;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
@@ -21,10 +22,10 @@ public class UpdateForwarder implements PluginMessageListener {
 
     /**
      * GemsEconomy Bungee-Spigot Messaging Listener
-     *
-     * This listener is used to update currencies and balance for players
-     * on different servers. This is important to sustain synced balances
-     * and currencies on all of the servers.
+     * <p>
+     * This listener is used to update currencies and balance for players on
+     * different servers. This is important to sustain synced balances and
+     * currencies on all the servers.
      */
 
     private final GemsEconomy plugin;
@@ -35,36 +36,35 @@ public class UpdateForwarder implements PluginMessageListener {
     }
 
     @Override
-    public void onPluginMessageReceived(String channel, Player notInUse, byte[] message) {
+    public void onPluginMessageReceived(String channel, @NotNull Player notInUse, byte[] message) {
         if (!channel.equals("BungeeCord")) {
             return;
         }
 
         ByteArrayDataInput in = ByteStreams.newDataInput(message);
-        String subchannel = in.readUTF();
-        if (subchannel.equals(channelName)) {
+        String subChannel = in.readUTF();
+        if (subChannel.equals(channelName)) {
             String[] info = in.readUTF().split(",");
             String type = info[0];
             String name = info[1];
-            if(plugin.isDebug()){
+            if (plugin.isDebug()) {
                 UtilServer.consoleLog(channelName + " - Received: " + type + " = " + name);
             }
 
-            if(type.equals("currency")){
+            if (type.equals("currency")) {
                 UUID uuid = UUID.fromString(name);
                 Currency currency = plugin.getCurrencyManager().getCurrency(uuid);
-                if(currency != null) {
+                if (currency != null) {
                     plugin.getDataStore().updateCurrencyLocally(currency);
-                    if(GemsEconomy.getInstance().isDebug()){
+                    if (GemsEconomy.getInstance().isDebug()) {
                         UtilServer.consoleLog(channelName + " - Currency " + name + " updated.");
                     }
                 }
-            }
-            else if(type.equals("account")){
+            } else if (type.equals("account")) {
                 UUID uuid = UUID.fromString(name);
                 plugin.getAccountManager().removeAccount(uuid);
                 SchedulerUtils.runAsync(() -> plugin.getDataStore().loadAccount(uuid));
-                if(plugin.isDebug()){
+                if (plugin.isDebug()) {
                     UtilServer.consoleLog(channelName + " - Account " + name + " updated.");
                 }
             }
@@ -72,7 +72,7 @@ public class UpdateForwarder implements PluginMessageListener {
     }
 
 
-    public void sendUpdateMessage(String type, String name){
+    public void sendUpdateMessage(String type, String name) {
         ByteArrayDataOutput out = ByteStreams.newDataOutput();
         out.writeUTF("Forward");
         out.writeUTF("ONLINE");
@@ -82,15 +82,15 @@ public class UpdateForwarder implements PluginMessageListener {
         DataOutputStream msgout = new DataOutputStream(msgbytes);
         try {
             msgout.writeUTF(type + "," + name);
-        } catch (IOException exception){
+        } catch (IOException exception) {
             exception.printStackTrace();
         }
 
         out.write(msgbytes.toByteArray());
 
         Player player = Iterables.getFirst(Bukkit.getOnlinePlayers(), null);
-        if(player == null){
-            if(GemsEconomy.getInstance().isDebug()){
+        if (player == null) {
+            if (GemsEconomy.getInstance().isDebug()) {
                 UtilServer.consoleLog(channelName + " - Player is Null. Cancelled.");
             }
             return;
