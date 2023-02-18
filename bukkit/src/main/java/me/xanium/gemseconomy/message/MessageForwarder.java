421b54b057b50f5d6d658f82af0a54930d489c06
Akiranya <porridgen@gmail.com>
2023-02-18 20:33:43 +0800

    Fix loading failure if ConnectorPlugin not present

--- bukkit/src/main/java/me/xanium/gemseconomy/message/MessageForwarder.java
+++ bukkit/src/main/java/me/xanium/gemseconomy/message/MessageForwarder.java
@@ -4,17 +4,19 @@ import de.themoep.connectorplugin.bukkit.BukkitConnectorPlugin;
 import me.xanium.gemseconomy.GemsEconomy;
 import me.xanium.gemseconomy.message.impl.DummyMessageForwarder;
 import me.xanium.gemseconomy.message.impl.RedisMessageForwarder;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
 
 import java.util.UUID;
 
 public interface MessageForwarder {
 
     static MessageForwarder get() {
-        BukkitConnectorPlugin connector = GemsEconomy.getInstance().getPlugin("ConnectorPlugin", BukkitConnectorPlugin.class);
+        Plugin connector = Bukkit.getServer().getPluginManager().getPlugin("ConnectorPlugin");
         if (connector == null) {
             return new DummyMessageForwarder();
         }
-        return new RedisMessageForwarder(GemsEconomy.getInstance(), connector);
+        return new RedisMessageForwarder(GemsEconomy.getInstance(), (BukkitConnectorPlugin) connector);
     }
 
     void sendMessage(String type, UUID uuid);
