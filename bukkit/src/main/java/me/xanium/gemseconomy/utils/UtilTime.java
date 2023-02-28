b5ce211f336cc078246628621f8572f94c402c42
Akiranya <porridgen@gmail.com>
2023-02-28 23:41:21 +0800

    It's Beijing timezone

--- bukkit/src/main/java/me/xanium/gemseconomy/utils/UtilTime.java
+++ bukkit/src/main/java/me/xanium/gemseconomy/utils/UtilTime.java
@@ -17,38 +17,37 @@ import java.util.TimeZone;
 @SuppressWarnings("unused")
 public class UtilTime {
 
-    public static final String DATE_FORMAT_NOW = "dd/MM/yyyy HH:mm:ss";
-    public static final String DATE_FORMAT_DAY = "dd/MM/yyyy";
+    public static final String DATE_FORMAT_NOW = "yyyy/MM/dd HH:mm:ss";
+    public static final String DATE_FORMAT_DAY = "yyyy/MM/dd";
 
     public static String now() {
         final Calendar cal = Calendar.getInstance();
-        final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        final SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT_NOW);
         return sdf.format(cal.getTime());
     }
 
     public static String when(final long time) {
-        final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        final SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT_NOW);
         return sdf.format(time);
     }
 
     public static String date() {
         final Calendar cal = Calendar.getInstance();
-        final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        final SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT_DAY);
         return sdf.format(cal.getTime());
     }
 
     public static String clock(boolean seconds) {
-        TimeZone zone = TimeZone.getTimeZone("Europe/Oslo");
+        TimeZone zone = TimeZone.getDefault();
         Date date = new Date();
+        SimpleDateFormat sdf;
         if (seconds) {
-            SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
-            sdf.setTimeZone(zone);
-            return sdf.format(date);
+            sdf = new SimpleDateFormat("HH:mm:ss");
         } else {
-            SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
-            sdf.setTimeZone(zone);
-            return sdf.format(date);
+            sdf = new SimpleDateFormat("HH:mm");
         }
+        sdf.setTimeZone(zone);
+        return sdf.format(date);
     }
 
     public static boolean elapsed(final long from, final long required) {
@@ -56,10 +55,9 @@ public class UtilTime {
     }
 
     public static String getCurrentDay() {
-        SimpleDateFormat dayFormat = new SimpleDateFormat("EEEE", Locale.US);
+        SimpleDateFormat dayFormat = new SimpleDateFormat("EEEE", Locale.CHINESE);
         Calendar calendar = Calendar.getInstance();
         return dayFormat.format(calendar.getTime());
-
     }
 
 }
